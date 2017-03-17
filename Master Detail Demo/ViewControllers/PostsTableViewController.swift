1f4c0f5297b80c24b95ae77496f2a5c6bed7fff2
Tim Richardson <tim@timrichardson.co>
2017-03-17 19:21:06 +0100

    Small update

--- Master Detail Demo/ViewControllers/PostsTableViewController.swift
+++ Master Detail Demo/ViewControllers/PostsTableViewController.swift
@@ -36,22 +36,20 @@ extension PostsTableViewController: NSFetchedResultsControllerDelegate {
     }
     
     func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
+        guard let indexPath = indexPath else { fatalError("Index path should be not nil") }
+        
         switch type {
         case .insert:
-            guard let indexPath = newIndexPath else { fatalError("Index path should be not nil") }
             tableView.insertRows(at: [indexPath], with: .fade)
         case .update:
-            guard let indexPath = indexPath else { fatalError("Index path should be not nil") }
             let post = fetchedResultsController?.object(at: indexPath)
             guard let cell = tableView.cellForRow(at: indexPath) else { break }
             cell.textLabel?.text = post?.title
         case .move:
-            guard let indexPath = indexPath else { fatalError("Index path should be not nil") }
             guard let newIndexPath = newIndexPath else { fatalError("New index path should be not nil") }
             tableView.deleteRows(at: [indexPath], with: .fade)
             tableView.insertRows(at: [newIndexPath], with: .fade)
         case .delete:
-            guard let indexPath = indexPath else { fatalError("Index path should be not nil") }
             tableView.deleteRows(at: [indexPath], with: .fade)
         }
     }
@@ -76,7 +74,7 @@ extension PostsTableViewController {
 
 extension PostsTableViewController {
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return 0
+        return 1
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
