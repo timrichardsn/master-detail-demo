bee28130da15af1bf253de315e25b91e522ad7e3
Tim Richardson <tim@timrichardson.co>
2017-03-17 17:01:00 +0100

    Added fetched results controller to the posts view controller

--- Master Detail Demo/ViewControllers/PostsTableViewController.swift
+++ Master Detail Demo/ViewControllers/PostsTableViewController.swift
@@ -11,7 +11,8 @@ import CoreData
 
 class PostsTableViewController: UITableViewController {
 
-    var managedObjectContext:NSManagedObjectContext?
+    var managedObjectContext:NSManagedObjectContext!
+    var fetchedResultsController:NSFetchedResultsController<Post>?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,8 +21,9 @@ class PostsTableViewController: UITableViewController {
         request.fetchBatchSize = 20
         request.returnsObjectsAsFaults = false
         
-        
-        //let frc = NSFetchedResultsController(fetchRequest: request, managedObjectContext: managedObjectContext, sectionNameKeyPath: nil, cacheName: nil)
+        fetchedResultsController = NSFetchedResultsController(fetchRequest: request, managedObjectContext: managedObjectContext, sectionNameKeyPath: nil, cacheName: nil)
+        fetchedResultsController?.delegate = self
+        try! fetchedResultsController?.performFetch()
     }
 
     // MARK: - Table view data source
@@ -31,7 +33,23 @@ class PostsTableViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        guard let section = fetchedResultsController?.sections?[section] else { return 0 }
+        return section.numberOfObjects
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let object = fetchedResultsController?.object(at: indexPath)
+        // unimplemented cell type: TODO
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "", for: indexPath) as? UITableViewCell else { fatalError("Unable to create cell") }
+        return cell
     }
 
+}
+
+//MARK: - NSFetchedResultsControllerDelegate
+
+extension PostsTableViewController: NSFetchedResultsControllerDelegate {
+    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        
+    }
 }
